d0dfce756d111d12a6260db7c38f660aae9c080a
DAN <87706283+ItsMeD4N@users.noreply.github.com>
2025-12-12 08:51:24 +0700

    fix

--- client/src/pages/LoginAdminPage.tsx
+++ client/src/pages/LoginAdminPage.tsx
@@ -52,7 +52,7 @@ const LoginAdminPage = () => {
                     onChange={(e) => setEmail(e.target.value)} 
                     required
                     className="w-full px-4 py-3 bg-slate-50 border border-slate-200 rounded-xl focus:ring-2 focus:ring-emerald-500 focus:border-transparent outline-none transition-all text-sm"
-                    placeholder="admin@hms.com"
+                    placeholder="email"
                 />
             </div>
             <div>
